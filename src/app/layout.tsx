45987be5cfa2466df4824d18b81f61fe463adc03
LeeChaeng <htijl@naver.com>
2022-11-05 16:12:14 +0900

    feat: add global style and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,25 @@
 import { FC, ReactNode } from "react";
+import "../styles/globalStyle.css";
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <html>
+      <head>
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+          href={"/font/SpoqaHanSansNeo-Bold.woff2"}
+        />
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+          href={"/font/SpoqaHanSansNeo-Regular.woff2"}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
