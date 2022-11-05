c0fc76f670f140cc73da27b004e095beb1b27e71
LeeChaeng <htijl@naver.com>
2022-11-05 16:07:15 +0900

    chore: add root head file

---
+++ src/app/head.tsx
@@ -0,0 +1,23 @@
+const Head = () => {
+  return (
+    <head>
+      <title>우당탕탕 개발 블로그</title>
+      <meta property="og:title" content="chaeng.dev" />
+      <meta name="twitter:title" content="chaeng.dev" />
+      <meta
+        name="content"
+        content="프론트엔드 개발자 루나의 개발 블로그입니다."
+      />
+      <meta
+        property="og:description"
+        content="프론트엔드 개발자 루나의 개발 블로그입니다."
+      />
+      <meta
+        name="twitter:description"
+        content="프론트엔드 개발자 루나의 개발 블로그입니다."
+      />
+    </head>
+  );
+};
+
+export default Head;
