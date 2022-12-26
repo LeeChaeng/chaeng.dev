9f91949401b121d1f696bf1d0ca789fc4f3255fe
Luna Lee <htijl@naver.com>
2022-12-26 23:52:19 +0900

    feat: detail layout

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,6 +2,9 @@ import path from "path";
 import { getAllPosts, POSTS_PATH } from "~/lib/api";
 import * as fs from "fs";
 import matter from "gray-matter";
+import Link from "next/link";
+import dayjs from "dayjs";
+import Image from "next/image";
 
 const getPost = (slug: string) => {
   const postFilePath = path.join(POSTS_PATH, `${slug}.mdx`);
@@ -15,7 +18,71 @@ const getPost = (slug: string) => {
 const PostPage = ({ params }: { params?: { slug?: string } }) => {
   const { content, data } = getPost(params?.slug ?? "");
 
-  return <div>{content}</div>;
+  return (
+    <main
+      className={
+        "flex flex-1 relative flex-col px-[32px] pt-[74px] max-w-[904px] self-center"
+      }
+    >
+      <div className={"flex flex-1 flex-col gap-[4px] mb-[32px]"}>
+        <Link href={"/"} className={"inline-block text-accent mt-[8px]"}>
+          HOME
+        </Link>
+        <span className={"text-[48px] font-bold"}>{data.title}</span>
+        <span className={"text-text font-light"}>
+          {dayjs(data.createdAt).format("MMMM DD, YYYY")}
+        </span>
+      </div>
+
+      {content}
+
+      <div
+        className={
+          "flex flex-col gap-[16px] tablet:flex-row tablet:gap-[32px] mx-[16px] mt-[40px] tablet:mt-[60px]"
+        }
+      >
+        <div className={"flex flex-col items-center gap-[4px] self-center"}>
+          <Image
+            alt="profile"
+            width={72}
+            height={72}
+            src={"/profile.jpeg"}
+            className={"rounded-full"}
+          />
+          <div className={"flex flex-col items-center"}>
+            <span className={"text-[16px] font-bold text-title"}>
+              이채영 (Luna Lee)
+            </span>
+            <span className={"text-[12px] font-normal text-text"}>
+              Frontend Engineer
+            </span>
+          </div>
+        </div>
+
+        <div className={"min-w-[300] tablet:min-w-[400]"}>
+          <h1 className={"text-[24px] font-bold text-title pb-[8px]"}>
+            <Link href={"/"} passHref>
+              chaeng.dev
+            </Link>
+          </h1>
+          <span className={"text-[14px] font-normal text-text break-words"}>
+            우당탕탕 프론트엔드 개발자 루나의 블로그
+          </span>
+
+          <div className={"flex gap-[8px] pt-[8px] pb-[8px]"}>
+            <Link href={"https://github.com/leechaeng"} passHref>
+              {/*<GitHub title="github" width={24} height={24} />*/}
+              Github
+            </Link>
+            <Link href={"https://www.linkedin.com/in/leechaeng/"} passHref>
+              {/*<LinkedIn title="linkedin" width={24} height={24} />*/}
+              LinkedIn
+            </Link>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export async function generateStaticParams() {
