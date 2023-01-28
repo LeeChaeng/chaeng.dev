9e2738b257d2f2b1ff1acbcecf2fdbd4c981c3cb
LeeChaeng <htijl@naver.com>
2023-01-28 16:28:09 +0900

    feat: add remark

--- src/component/markdownRenderer.tsx
+++ src/component/markdownRenderer.tsx
@@ -1,30 +1,25 @@
 import { FC } from "react";
-import ReactMarkdown from "react-markdown";
-import {
-  Heading1,
-  Heading2,
-  Heading3,
-  Heading4,
-  Heading5,
-  Heading6,
-} from "~/component/customTag/heading";
+import { remark } from "remark";
+import remarkBreaks from "remark-breaks";
+import sanitizeHtml from "sanitize-html";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypeFormat from "rehype-format";
 
 const MarkdownRenderer: FC<{ children: string }> = ({ children }) => {
-  return (
-    <ReactMarkdown
-      components={{
-        h1: ({ children }) => <Heading1>{children}</Heading1>,
-        h2: ({ children }) => <Heading2>{children}</Heading2>,
-        h3: ({ children }) => <Heading3>{children}</Heading3>,
-        h4: ({ children }) => <Heading4>{children}</Heading4>,
-        h5: ({ children }) => <Heading5>{children}</Heading5>,
-        h6: ({ children }) => <Heading6>{children}</Heading6>,
-        p: ({ children }) => <p>{children}</p>,
-      }}
-    >
-      {children}
-    </ReactMarkdown>
-  );
+  const filter = (html: string) => {
+    return remark()
+      .use(remarkBreaks)
+      .use(remarkParse)
+      .use(remarkRehype)
+      .use(rehypeFormat)
+      .use(rehypeStringify)
+      .processSync(html)
+      .toString();
+  };
+
+  return <div dangerouslySetInnerHTML={{ __html: filter(children) }} />;
 };
 
 export { MarkdownRenderer };
