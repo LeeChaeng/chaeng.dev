86885ec8c2ed38ed7059957b2dda8fa9937f265c
LeeChaeng <htijl@naver.com>
2023-03-15 16:19:19 +0900

    chore: fix some style

--- src/component/post.tsx
+++ src/component/post.tsx
@@ -19,38 +19,36 @@ const Post: FC<PostProps> = ({
   slug,
 }) => {
   return (
-    <article className={"max-w-[704px]"}>
-      <Link href={`/posts/${slug}`}>
-        <time dateTime={createdAt} className={"text-[14px] text-text"}>
-          {dayjs(createdAt).format("MMMM DD, YYYY")}
-        </time>
+    <Link href={`/posts/${slug}`} className={"max-w-[704px]"}>
+      <time dateTime={createdAt} className={"text-[14px] text-text"}>
+        {dayjs(createdAt).format("MMMM DD, YYYY")}
+      </time>
 
-        <h2 className={"mb-[16px] break-words text-title"}>{title}</h2>
+      <h2 className={"mb-[16px] break-words text-title"}>{title}</h2>
 
-        <div className={"flex flex-col tablet:flex-row gap-[24px]"}>
-          <p className={"flex-1 flex flex-col order-2 tablet:order-1"}>
-            {summary}
-            <span className={"text-[15px] text-accent"}>Read More</span>
-          </p>
+      <div className={"flex flex-col tablet:flex-row gap-[24px]"}>
+        <p className={"flex-1 flex flex-col order-2 tablet:order-1"}>
+          {summary}
+          <span className={"text-[15px] text-accent mt-[4px]"}>Read More</span>
+        </p>
 
-          {/*TODO: alt*/}
-          {thumbnail && (
-            <div
-              className={
-                "flex-1 relative aspect-[5/3] rounded-lg drop-shadow-lg order-1 tablet:order-2"
-              }
-            >
-              <Image
-                src={`https://images.chaeng.dev/${thumbnail}`}
-                alt={"post example"}
-                fill={true}
-                className={"object-cover rounded-lg"}
-              />
-            </div>
-          )}
-        </div>
-      </Link>
-    </article>
+        {/*TODO: alt*/}
+        {thumbnail && (
+          <div
+            className={
+              "flex-1 relative aspect-[5/3] rounded-lg drop-shadow-lg order-1 tablet:order-2"
+            }
+          >
+            <Image
+              src={`https://images.chaeng.dev/${thumbnail}`}
+              alt={"post example"}
+              fill={true}
+              className={"object-cover rounded-lg"}
+            />
+          </div>
+        )}
+      </div>
+    </Link>
   );
 };
 
