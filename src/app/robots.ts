513a5765d6ac9fde4d19e518e9153ca23f8ca91d
LeeChaeng <htijl@naver.com>
2023-04-07 23:45:38 +0900

    feat: delete public folder's robots and use MetadataRoute.Robots

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from 'next';
+
+const robots = (): MetadataRoute.Robots => {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://chaeng.dev/sitemap.xml',
+  };
+};
+
+export default robots;
